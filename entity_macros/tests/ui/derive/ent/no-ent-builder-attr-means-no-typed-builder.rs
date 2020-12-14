7cecd10cd09fe89d5740668ab9e00ad939f8f43a
Chip Senkbeil <chip@senkbeil.org>
2020-12-14 00:04:06 -0600

    Add ui tests for validating that builder and query are not generated without their associated ent(...) attributes

---
+++ entity_macros/tests/ui/derive/ent/no-ent-builder-attr-means-no-typed-builder.rs
@@ -0,0 +1,20 @@
+use entity::{Database, Ent, Id};
+
+#[derive(Clone, Ent)]
+struct TestEnt {
+    #[ent(id)]
+    id: Id,
+
+    #[ent(database)]
+    database: Option<Box<dyn Database>>,
+
+    #[ent(created)]
+    created: u64,
+
+    #[ent(last_updated)]
+    last_updated: u64,
+}
+
+fn main() {
+    let _ = TestEntBuilder::default();
+}
