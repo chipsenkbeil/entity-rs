3f54a0ffc0295f3c67e1c30c2e4db0209ad6f41d
Chip Senkbeil <chip@senkbeil.org>
2021-01-02 01:30:21 +0000

    Add inmemory example

---
+++ examples/inmemory/src/main.rs
@@ -0,0 +1,44 @@
+use entity::{simple_ent, Ent, Id, InmemoryDatabase};
+
+#[simple_ent(debug)]
+struct User {
+    name: String,
+    age: u8,
+
+    #[ent(edge(type = "Address"))]
+    address: Id,
+}
+
+#[simple_ent(debug)]
+struct Address {
+    street: String,
+    city: String,
+    zipcode: String,
+    state: String,
+}
+
+fn main() {
+    let db = InmemoryDatabase::default();
+    entity::global::set_db(db);
+
+    let mut address = Address::build()
+        .street("123 Some Street".to_string())
+        .city("Some City".to_string())
+        .zipcode("12345".to_string())
+        .state("SW".to_string())
+        .build()
+        .unwrap();
+
+    address.commit().unwrap();
+    println!("{:?}", address);
+
+    let mut user = User::build()
+        .name("abc".to_string())
+        .age(31)
+        .address(address.id())
+        .build()
+        .unwrap();
+
+    user.commit().unwrap();
+    println!("{:?}", user);
+}
