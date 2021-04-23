c2c4e45278500eafe412f8883b107c18f0494971
Chip Senkbeil <chip@senkbeil.org>
2021-04-22 22:35:02 -0500

    Add quiet mode to release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -20,6 +20,7 @@ CHANGELOG="$DIR/../CHANGELOG.md"
 ROOT_CARGO_TOML="$DIR/../Cargo.toml"
 DRY_RUN=1
 VERBOSE=0
+QUIET=0
 GIT_BRANCH=main
 SKIP_CHANGELOG=0
 SKIP_GIT_TAG=0
@@ -45,12 +46,18 @@ sedi () {
   fi
 }
 
+print_msg () {
+  if [ "$QUIET" -eq 0 ]; then
+    echo "$@"
+  fi
+}
+
 # NOTE: We want to detect the current version without diff
 TARGET_VERSION="$(sedi {GET} -n "s/^version = \"\([^\"]*\)\"/\1/p" "$ROOT_CARGO_TOML")"
 NEXT_VERSION=
 
 function usage {
-  echo "Usage: $(basename $0) [-vfh] [-s STEP] [-b BRANCH] [-t TARGET_VERSION] [-n NEXT_VERSION]" 2>&1
+  echo "Usage: $(basename $0) [-vfhq] [-s STEP] [-b BRANCH] [-t TARGET_VERSION] [-n NEXT_VERSION]" 2>&1
   echo 'Release the current version of entity crates.'
   echo ''
   echo "   -t VERSION  Specify target version to use (default = $TARGET_VERSION)"
@@ -61,13 +68,15 @@ function usage {
   echo '   -f          Force release, rather than performing dry run'
   echo '   -h          Print this help information'
   echo '   -v          Increase verbosity'
+  echo '   -q          Suppress output (quiet)'
   exit 1
 }
 
-while getopts ':vfht:n:s:b:' arg; do
+while getopts ':vfhqt:n:s:b:' arg; do
   case "${arg}" in
     t) TARGET_VERSION=${OPTARG};;
     n) NEXT_VERSION=${OPTARG};;
+    q) QUIET=1;;
     v) VERBOSE=1;;
     f) DRY_RUN=0;;
     b) GIT_BRANCH=${OPTARG};;
@@ -109,9 +118,9 @@ shift "$OPTIND"
 # 3. Add new unreleased template at top of changelog
 # 4. Commit all changes in git
 if [ "$SKIP_CHANGELOG" -eq 1 ]; then
-  echo 'Skipping changelog updates!'
+  print_msg 'Skipping changelog updates!'
 elif [ -n "$TARGET_VERSION" ]; then
-  echo "[$TARGET_VERSION]: $CHANGELOG"
+  print_msg "[$TARGET_VERSION]: $CHANGELOG"
   sedi "s/Unreleased/$TARGET_VERSION/g" "$CHANGELOG"
   sedi "s/ReleaseDate/$(date "+%Y-%m-%d")/g" "$CHANGELOG"
   sedi "s/<!-- next-header -->/<!-- next-header -->\n\n## [Unreleased] - ReleaseDate/g" "$CHANGELOG"
@@ -121,36 +130,41 @@ elif [ -n "$TARGET_VERSION" ]; then
     git add --all
     git commit -m "[Release $TARGET_VERSION] Changelog updates"
   else
-    echo 'git add --all'
-    echo "git commit -m \"[Release $TARGET_VERSION] Changelog updates\""
+    print_msg 'git add --all'
+    print_msg "git commit -m \"[Release $TARGET_VERSION] Changelog updates\""
   fi
 else
-  echo 'Target version not provided! Skipping CHANGELOG.md updates & tagging!'
+  print_msg 'Target version not provided! Skipping CHANGELOG.md updates & tagging!'
 fi
 
 # Publish each crate with current version
 if [ "$SKIP_CARGO_PUBLISH" -eq 0 ]; then
   for crate in "${CRATES[@]}"; do
     dry_run_arg=
-    if [ "$DRY_RUN" -gt 0 ]; then
+    if [ "$DRY_RUN" -eq 1 ]; then
       dry_run_arg=--dry-run
     fi
 
-    cargo hack publish -p "$crate" --no-dev-deps --allow-dirty $dry_run_arg
+    quiet_arg=
+    if [ "$QUIET" -eq 1 ]; then
+      quiet_arg=--quiet
+    fi
+
+    cargo hack publish -p "$crate" --no-dev-deps --allow-dirty $dry_run_arg $quiet_arg
   done
 else
-  echo 'Skipping Cargo crate publishing!'
+  print_msg 'Skipping Cargo crate publishing!'
 fi
 
 # Update all Cargo.toml with version change for entity-related crates
 # 1. Replace crate's version with new version
 # 2. Replace dependency crates' versions with new version
 if [ "$SKIP_CARGO_TOML_UPDATE" -eq 1 ]; then
-  echo 'Skipping Cargo.toml updates!'
+  print_msg 'Skipping Cargo.toml updates!'
 elif [ -n "$NEXT_VERSION" ]; then
   CARGO_TOML_FILES=($(find "$DIR/.." -name "Cargo.toml"))
   for cargo_toml in "${CARGO_TOML_FILES[@]}"; do
-    echo "[$TARGET_VERSION -> $NEXT_VERSION]: $cargo_toml"
+    print_msg "[$TARGET_VERSION -> $NEXT_VERSION]: $cargo_toml"
     sedi "1,/^version/ s/^version = \".*\"/version = \"$NEXT_VERSION\"/g" "$cargo_toml"
     sedi "s/^\(entity.*version = \"\)[^\"]*\(\".*\)$/\1=$NEXT_VERSION\2/g" "$cargo_toml"
   done
@@ -160,21 +174,21 @@ elif [ -n "$NEXT_VERSION" ]; then
     git add --all
     git commit -m "[Release $TARGET_VERSION] Bump to next version ($NEXT_VERSION)"
   else
-    echo 'git add --all'
-    echo "git commit -m \"[Release $TARGET_VERSION] Bump to next version ($NEXT_VERSION)\""
+    print_msg 'git add --all'
+    print_msg "git commit -m \"[Release $TARGET_VERSION] Bump to next version ($NEXT_VERSION)\""
   fi
 else
-  echo 'Next version not provided! Skipping Cargo.toml updates!'
+  print_msg 'Next version not provided! Skipping Cargo.toml updates!'
 fi
 
 if [ "$SKIP_GIT_TAG" -eq 0 ]; then
   if [ "$DRY_RUN" -eq 0 ]; then
     git tag "v$TARGET_VERSION"
   else
-    echo "git tag \"v$TARGET_VERSION\""
+    print_msg "git tag \"v$TARGET_VERSION\""
   fi
 else
-  echo 'Skipping git tagging!'
+  print_msg 'Skipping git tagging!'
 fi
 
 if [ "$DRY_RUN" -eq 0 ]; then
@@ -186,9 +200,9 @@ if [ "$DRY_RUN" -eq 0 ]; then
   fi
 else
   if [ "$SKIP_GIT_PUSH" -eq 0 ]; then
-    echo "git push origin \"$GIT_BRANCH\""
+    print_msg "git push origin \"$GIT_BRANCH\""
     if [ "$SKIP_GIT_TAG" -eq 0 ]; then
-      echo "git push origin \"v$TARGET_VERSION\""
+      print_msg "git push origin \"v$TARGET_VERSION\""
     fi
   fi
 fi
